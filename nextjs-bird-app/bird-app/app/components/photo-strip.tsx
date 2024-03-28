9439f7ee6486cf563999a7acaa0cb30bfd18850a
madibentl <mb.madisonbentley@gmail.com>
2024-03-28 08:35:49 -0400

    make working carousel

--- nextjs-bird-app/bird-app/app/components/photo-strip.tsx
+++ nextjs-bird-app/bird-app/app/components/photo-strip.tsx
@@ -8,12 +8,15 @@ export default function PhotoStrip(){
     const photos = getPhotos();
 
     return (
-        <Carousel>
-        {photos.map(photo => (
-            <li key={photo.id} className='embla__slide'>
-                <PhotoCard {...photo}></PhotoCard>
-            </li>
-        ))}
-        </Carousel>
+        <section>
+            <h2 className="text-xl font-medium lg:text-2xl mx-auto mb-5">View a bird photo</h2>
+            <Carousel>
+            {photos.map(photo => (
+                <li key={photo.id} className='embla__slide'>
+                    <PhotoCard {...photo}></PhotoCard>
+                </li>
+            ))}
+            </Carousel>
+        </section>
     )
 }
